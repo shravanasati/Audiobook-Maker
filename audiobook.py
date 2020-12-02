6b5bce8c4b42ae714298e3c5a3cf84f26b629d19
Shravan <69118069+Shravan-1908@users.noreply.github.com>
2020-12-02 19:44:08 +0530

    Voice selection and internetless creation
    Users now have the freedom to choose the voice of the audio book (male/female). Also the audio book can now be created WITHOUT any internet connection.

--- audiobook.py
+++ audiobook.py
@@ -1,9 +1,8 @@
 # pypdf2 for extracting text form pdf, docx for word documents, gtts for text to speech conversion and os for checking if the path is valid and starting the file
 from docx import Document
-from gtts import gTTS
-from playsound import playsound
+import pyttsx3
 from PyPDF2 import PdfFileReader
-from os import path as ospath
+from os.path import exists
 from os import startfile
 
 
@@ -36,7 +35,7 @@ class AudioBook():
         """
         Checks whether the given path is valid or not.
         """
-        if not ospath.exists(self.stream):
+        if not exists(self.stream):
             print("The given path is invalid.")
             quit()
 
@@ -48,8 +47,18 @@ class AudioBook():
         if start.lower() == "y":
             startfile(self.audiofile)
 
+    def convert_to_audio(self, text:str, voice:str="male"):
+        """
+        Converts the given string into audio.
+        """
+        engine = pyttsx3.init()
+        if voice == "female":
+            engine.setProperty('voice', r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0")
+        engine.save_to_file(text, self.audiofile)
+        engine.runAndWait()
 
-    def text_to_speech_PDF(self):
+
+    def text_to_speech_PDF(self, voice:str="male"):
         """
         Converts the given PDF text to speech.
         """
@@ -62,21 +71,18 @@ class AudioBook():
                 page_text = book.getPage(i).extractText()
                 text += page_text
 
-            print("Converting to audio... This may take a while depending on your internet speed and size of the book...")
+            print("Converting to audio... This may take a while depending on the size of the book...")
             # converting the text to audio and saving the audio file
-            speech = gTTS(text, lang='en')
-            speech.save(self.audiofile)
+            self.convert_to_audio(text, voice)
 
         except Exception as e:
-            playsound(r"C:\Users\Lenovo\Documents\Python Codes\Awkward Cricket.mp3")
             print(e)
 
         else:
-            playsound(r"C:\Users\Lenovo\Documents\Python Codes\anime wow.mp3")
             print("Your audiobook is ready!")
             self.listen()
 
-    def text_to_speech_TXT(self):
+    def text_to_speech_TXT(self, voice:str="male"):
         """
         Converts the text file into speech.
         """
@@ -85,20 +91,17 @@ class AudioBook():
             with open(self.stream) as f:
                 fc = f.read()
 
-            print("Converting to audio... This may take a while depending on your internet speed and size of the book...")    
-            speech = gTTS(fc, lang='en')
-            speech.save(self.audiofile)
+            print("Converting to audio... This may take a while depending the size of the book...")    
+            self.convert_to_audio(fc, voice)
         
         except Exception as e:
-            playsound(r"C:\Users\Lenovo\Documents\Python Codes\Awkward Cricket.mp3")
             print(e)
 
         else:
-            playsound(r"C:\Users\Lenovo\Documents\Python Codes\anime wow.mp3")
             print("Your audiobook is ready!")
             self.listen()
 
-    def text_to_speech_DOCX(self):
+    def text_to_speech_DOCX(self, voice:str="male"):
         """
         Converts the given word file into audio.
         """
@@ -110,36 +113,42 @@ class AudioBook():
             for para in doc.paragraphs:
                 text += para.text + "\n"
 
-            print("Converting to audio... This may take a while depending on your internet speed and size of the book...")
-            speech = gTTS(text, lang='en')
-            speech.save(self.audiofile)
+            print("Converting to audio... This may take a while depending on the size of the book...")
+            self.convert_to_audio(text, voice)
             
         except Exception as e:
-            playsound(r"C:\Users\Lenovo\Documents\Python Codes\Awkward Cricket.mp3")
             print(e)
 
         else:
-            playsound(r"C:\Users\Lenovo\Documents\Python Codes\anime wow.mp3")
             self.listen()
 
 
-    def create_audiobook(self):
+    def create_audiobook(self, voice):
         """
         Calls the isValid and text_to_speech functions in order to create the audiobook.
         """
         self.isValid()
 
         if self.form == "pdf":
-            self.text_to_speech_PDF()
+            self.text_to_speech_PDF(voice)
         elif self.form == "txt":
-            self.text_to_speech_TXT()
+            self.text_to_speech_TXT(voice)
         elif self.form == "docx":
-            self.text_to_speech_DOCX()
+            self.text_to_speech_DOCX(voice)
 
 
 if __name__ == "__main__":
     print("Welcome to the Python Audio Book Maker!")
+
     source = input("Enter the full path of the PDF/text/docx file you want to turn into audiobook: ")
     destination = input("Enter the name of the audiobook to save: ")
-    ab = AudioBook(source, destination)
-    ab.create_audiobook()
+    voice = input("In which audio do you want the audiobook to be?(male/female) :")
+
+    if voice.lower() not in ["male", "female"]:
+        print("You entered the wrong audio! Therefore, defaulting to male voice.")
+        ab = AudioBook(source, destination)
+        ab.create_audiobook("male")
+
+    else:
+        ab = AudioBook(source, destination)
+        ab.create_audiobook(voice)
